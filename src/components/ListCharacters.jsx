0914b3c0285744e4400b449c51ca6ca6b8ff97ec
Caleb Parrra <mccalebyt@gmail.com>
2022-06-27 11:46:52 -0500

    loading

--- src/components/ListCharacters.jsx
+++ src/components/ListCharacters.jsx
@@ -5,11 +5,13 @@ import Character from './Character'
 
 export default function ListCharacters() {
     const [characters, setCharacters] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         async function fecthData() {
             const response = await fetch('https://rickandmortyapi.com/api/character');
             const data = await response.json();
+            setLoading(false);
             setCharacters(data.results)
         }
         fecthData()
@@ -18,11 +20,13 @@ export default function ListCharacters() {
     return (
         <>
             {
-                characters.map(character => {
+                loading ? (<h1>Cargando</h1>) : 
+                
+                (characters.map(character => {
                     return (
-                        <Character key={character.id} character={character}/>
+                        <Character key={character.id} character={character} />
                     )
-                })
+                }))
             }
         </>
 
